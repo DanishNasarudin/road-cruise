7e042732a608f2452f90ecd749891f2cf14ffa9f
Khai <khaiwern_3@hotmail.com>
2024-11-02 20:33:06 +0800

    chore: update env naming

--- db/db.ts
+++ db/db.ts
@@ -2,10 +2,8 @@ import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import * as schema from "./schema";
 
-config({ path: ".env" });
+config({ path: ".env.local" });
 
-const { DATABASE_URL } = process.env;
-
-const db = drizzle(DATABASE_URL!, { schema });
+const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 export default db;
